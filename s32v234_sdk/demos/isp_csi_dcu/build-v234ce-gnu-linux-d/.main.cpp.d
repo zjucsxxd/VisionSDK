011bd3bedaaab6a875391f12b02e0fde7e817f6c
ecocar <uw.infotainment@gmail.com>
2016-03-12 15:35:17 -0800

    script to build and send

--- s32v234_sdk/demos/isp_csi_dcu/build-v234ce-gnu-linux-d/.main.cpp.d
+++ s32v234_sdk/demos/isp_csi_dcu/build-v234ce-gnu-linux-d/.main.cpp.d
@@ -57,7 +57,30 @@ main-cpp.o .main.cpp.d: ../src/main.cpp \
  ../../../../s32v234_sdk/libs/arm/isp/sequencer/kernel/include/seq.h \
  ../../../../s32v234_sdk/libs/arm/io/frame_io/include/frame_output_v234fb.h \
  ../../../../s32v234_sdk/libs/arm/io/frame_io/include/frame_output_base.h \
- ../../../../s32v234_sdk/libs/arm/io/frame_io/include/video/fsl_dcu_ioctl.h
+ ../../../../s32v234_sdk/libs/arm/io/frame_io/include/video/fsl_dcu_ioctl.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/opencv.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/core/core_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/miniflann.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/defines.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/config.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/imgproc_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/types_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/imgproc.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/photo/photo.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/photo/photo_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/video.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/tracking.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/background_segm.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/features2d/features2d.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/objdetect/objdetect.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/calib3d/calib3d.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/core/affine.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/ml/ml.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/highgui/highgui_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/highgui/highgui.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/contrib.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/retina.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/openfabmap.hpp
 #### dont care rules
 ../src/main.cpp \
  ../../../../s32v234_sdk/libs/arm/io/frame_io/include/frame_input_patterngen.h \
@@ -118,4 +141,27 @@ main-cpp.o .main.cpp.d: ../src/main.cpp \
  ../../../../s32v234_sdk/libs/arm/isp/sequencer/kernel/include/seq.h \
  ../../../../s32v234_sdk/libs/arm/io/frame_io/include/frame_output_v234fb.h \
  ../../../../s32v234_sdk/libs/arm/io/frame_io/include/frame_output_base.h \
- ../../../../s32v234_sdk/libs/arm/io/frame_io/include/video/fsl_dcu_ioctl.h:
+ ../../../../s32v234_sdk/libs/arm/io/frame_io/include/video/fsl_dcu_ioctl.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/opencv.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/core/core_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/miniflann.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/defines.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/flann/config.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/imgproc_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/types_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/imgproc/imgproc.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/photo/photo.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/photo/photo_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/video.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/tracking.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/video/background_segm.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/features2d/features2d.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/objdetect/objdetect.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/calib3d/calib3d.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/core/affine.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/ml/ml.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/highgui/highgui_c.h \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/highgui/highgui.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/contrib.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/retina.hpp \
+ ../../../../s32v234_sdk/ocv/linux-arm64/include/opencv2/contrib/openfabmap.hpp:
---
+++ s32v234_sdk/demos/isp_csi_dcu/build-v234ce-gnu-linux-d/make_send.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make clean && make APEX_PREBUILD=1 allsub && scp isp_csi_dcu.elf root@128.95.116.88:/s32v234/demos
+
